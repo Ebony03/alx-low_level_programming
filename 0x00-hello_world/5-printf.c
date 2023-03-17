4a34613a4aca79c698712d4a222ff98ecdd2e982
Ebony03 <yuudy27@gmail.com>
2023-03-16 17:52:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a program using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{																				printf("but the outcome is a piece of art,\n");
+return (0);
+}
