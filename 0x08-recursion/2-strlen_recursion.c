f2c53957a4b9616a9b0b26cf5005667a54672006
Ebony03 <yuudy27@gmail.com>
2023-04-05 17:48:03 -0700

    3-factorial.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	long int = (0);
+
+	if (*s)
+	{
+		long int ++;
+		long int = _strlen_recursion(s + 1);
+
+	}
+
+	return (long int);
+
+}
